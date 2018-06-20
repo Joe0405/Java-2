dd2c90c9d4a65e16f6f0c8144c2c55f413ac8437
Joe0405 <joeabrahamk@yahoo.com>
2018-06-20 10:04:12 +0530

    Change to Factorial Program

--- src/pe/Factorial.java
+++ src/pe/Factorial.java
@@ -1,22 +1,33 @@
 package pe;
 
 public class Factorial {
-	public static int findIntFactorial(int a) {
-		int factorial = 1;
-		while(a>0) {
-			factorial*=a;
-			a--;		
+	public static int[] findIntFactorial(int factorial) {
+		int factorialArray[] = new int[factorial];
+		int i, temp;
+		for(i=factorial-1; i>=0; i--, factorial--) {
+			factorialArray[i] = 1;
+			temp = factorial;
+			while(temp>0) {
+				factorialArray[i]*=temp;
+				temp--;		
+			}
 		}
-		return factorial;
+		
+		return factorialArray;
 	}
 	
-	public static long findLongFactorial(int a) {
-		long factorial = (long) 1;
-		while(a>0) {
-			factorial*=a;
-			a--;	
+	public static long[] findLongFactorial(int factorial) {
+		long factorialArray[] = new long[factorial];
+		int i, temp;
+		for(i=factorial-1; i>=0; i--, factorial--) {
+			factorialArray[i] = 1;
+			temp = factorial;
+			while(temp>0) {
+				factorialArray[i]*=temp;
+				temp--;		
+			}
 		}
-		return factorial;
+		return factorialArray;
 	}
 	
 }
--- src/test/TestFactorial.java
+++ src/test/TestFactorial.java
@@ -20,16 +20,31 @@ public class TestFactorial {
 
 	@Test
 	public void test1() {
-		int expected = 2 ;
-		int actual = Factorial.findIntFactorial(2);
-		assertEquals(expected, actual);
+		int[] expected = {1,2,6,24};
+		int[] actual = Factorial.findIntFactorial(4);
+		assertEquals(expected[0], actual[0]);
+		assertEquals(expected[1], actual[1]);
+		assertEquals(expected[2], actual[2]);
+		assertEquals(expected[3], actual[3]);
 	}
 	
 	@Test
 	public void test2() {
-		long expected = 2432902008176640000L;
-		long actual =Factorial.findLongFactorial(20);
-		assertEquals(expected, actual);
+		long[] expected = {1,2,6,24,120,720,5040,40320,362880,3628800,39916800,479001600,6227020800L};
+		long[] actual =Factorial.findLongFactorial(13);
+		assertEquals(expected[0], actual[0]);
+		assertEquals(expected[1], actual[1]);
+		assertEquals(expected[2], actual[2]);
+		assertEquals(expected[3], actual[3]);
+		assertEquals(expected[4], actual[4]);
+		assertEquals(expected[5], actual[5]);
+		assertEquals(expected[6], actual[6]);
+		assertEquals(expected[7], actual[7]);
+		assertEquals(expected[8], actual[8]);
+		assertEquals(expected[9], actual[9]);
+		assertEquals(expected[10], actual[10]);
+		assertEquals(expected[11], actual[11]);
+		assertEquals(expected[12], actual[12]);
 	}
 
 }
